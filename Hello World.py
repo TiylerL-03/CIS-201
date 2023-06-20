206aacc4fee92d7f00455e8dc81401549803b682
Tiyler Lynum <137097035+TiylerL-03@users.noreply.github.com>
2023-06-20 10:00:45 -0700

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,16 @@
+"""
+Name: helloWorld
+Purpose: Introduction to Python
+Author: Tiyler Lynum
+"""
+
+
+print ("Hello World!")
+print ()
+message1 = "Hello World!"
+message2 = "I have a message for you!"
+message3 = "You are older than dirt."
+name = str(input("Type your name: "))
+age = int(input(" How old are you? "))
+print ("Good Morning," , name + ".", message2, message1)
+print (name + "," "you are" ,age, "years old.", message3)
