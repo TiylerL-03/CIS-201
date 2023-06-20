cde134e4da2117d98387baefdd1674e94fd1b51c
Tiyler Lynum <137097035+TiylerL-03@users.noreply.github.com>
2023-06-20 10:58:48 -0700

    Add files via upload

---
+++ aboutMe.py
@@ -0,0 +1,8 @@
+firstName = str(input("What is your First Name?"))
+lastName = str(input("What is your Last Name?"))
+age = int(input("How old are you?"))
+graduatingClass = str(input("When are you graduating?"))
+hobby = str(input(" What are your hobbies?"))
+relationshipStatus = str(input("Are you single, dating, in a relationship, or married?"))
+car = str(input("What kind of car do you have?"))
+print ("Hello. Your name is,",firstName+lastName,".""You are",age,"years old." "You are graduating in",graduatingClass,"and have",hobby,"as hobbies.""You are",relationshipStatus,"and drive a",car,".")
